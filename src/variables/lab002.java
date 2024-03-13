bfd6e19912847bec6e654524161f2e8523cb123a
RuchiVerma-bit <vermaruchi067@gmail.com>
2024-03-13 21:13:34 +0530

    First Commit

---
+++ src/variables/lab002.java
@@ -0,0 +1,15 @@
+package src.variables;
+
+public class lab002 {
+
+    public static void main(String[] args) {
+        byte age= 10;
+        short age1= 128;
+        int amount= 4000000;
+        long balance=987654123;
+        System.out.println("Age= " + age);
+        System.out.println("Age1= "+ age1);
+        System.out.println("Amount= "+ amount);
+        System.out.println("Balance= " + balance);
+    }
+}
